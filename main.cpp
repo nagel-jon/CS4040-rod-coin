8f6f666fb28f4b64b07968a4a2307c0421cc44a0
nagel-jon <jn855420@ohio.edu>
2023-11-16 14:33:15 -0500

    need to fix reconstruction tests

--- main.cpp
+++ main.cpp
@@ -23,13 +23,25 @@ Must cut down these lengths to a combination of lenghts in table
 
 int main() {
 
+  // create vectors for sparse cut
+
   vector<int> price = {1, 5, 9, 17, 20, 30, 36, 48}; // Sample price vector
 
   vector<int> lengths = {1, 2, 4, 6, 8, 10, 12, 16}; // Sample lengths vector
 
   vector<int> tests = {4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096};
 
-  cout << "Sparse Cut Tests ---------------" << endl;
+  // Sparse Cut Tests
+
+  cout << "Time for Single Call to Sparse Cut: ";
+  auto startTime = std::chrono::high_resolution_clock::now();
+  int result = sparse_cut(price, lengths, 1);
+  auto endTime = std::chrono::high_resolution_clock::now();
+  auto duration =
+      std::chrono::duration_cast<std::chrono::nanoseconds>(endTime - startTime);
+  std::cout << duration.count() << " nanoseconds." << std::endl << endl;
+
+  cout << "Sparse Cut Tests ---------------" << endl << endl;
   for (int i = 0; i < tests.size(); ++i) {
     auto startTime = std::chrono::high_resolution_clock::now();
     int result = sparse_cut(price, lengths, tests[i]);
@@ -42,29 +54,85 @@ int main() {
     std::cout << "Sparse Cut Result For: " << tests[i] << " -> " << result
               << std::endl;
   }
-    // coin change
 
-    int us_coins[] = {1, 5, 10, 25, 50, 100, 200, 500, 1000, 2000};
+  cout << endl;
+  // coin change
 
-    vector <int> us_experiments = {10, 50, 100, 500, 1000, 1500, 2000, 3000, 5000};
+  int us_coins[] = {1, 5, 10, 25, 50, 100, 200, 500, 1000, 2000};
 
-    int num_us_coins = sizeof(us_coins) / sizeof(us_coins[0]);
+  vector<int> coin_experiments = {10,   50,   100,  500, 1000,
+                                  1500, 2000, 3000, 5000};
 
-    cout << "Original Coin Change Tests ---------------" << endl;
+  int num_us_coins = sizeof(us_coins) / sizeof(us_coins[0]);
+
+  cout << "Original Coin Change Tests ---------------" << endl << endl;
+
+  for (int i = 0; i < coin_experiments.size(); ++i) {
+    auto startTime = std::chrono::high_resolution_clock::now();
+
+    int result =
+        original_coin_change(coin_experiments[i], us_coins, num_us_coins);
+
+    auto endTime = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(
+        endTime - startTime);
+
+    // Print the result and the time taken for each test
+    std::cout << "Experiment input: " << coin_experiments[i] << std::endl;
+    std::cout << "Result: " << result << std::endl;
+    std::cout << "Time taken: " << duration.count() << " nanoseconds"
+              << std::endl;
+  }
+  cout << endl;
+
+  int wizard_coins[] = {1, 29, 493};
+  int num_wizard_coins = sizeof(wizard_coins) / sizeof(wizard_coins[0]);
+
+  cout << "Wizard Currency Coin Change Tests ---------------" << endl << endl;
+  for (int i = 0; i < coin_experiments.size(); ++i) {
+    auto startTime = std::chrono::high_resolution_clock::now();
+
+    int result = original_coin_change(coin_experiments[i], wizard_coins,
+                                      num_wizard_coins);
+
+    auto endTime = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(
+        endTime - startTime);
+
+    // Print the result and the time taken for each test
+    std::cout << "Experiment input: " << coin_experiments[i] << std::endl;
+    std::cout << "Result: " << result << std::endl;
+    std::cout << "Time taken: " << duration.count() << " nanoseconds"
+              << std::endl;
+  }
+  cout << endl;
 
 
-    for (int i = 0; i < us_experiments.size(); ++i) {
-        auto startTime = std::chrono::high_resolution_clock::now();
-        
-        int result = original_coin_change(us_experiments[i], us_coins, num_us_coins);
-        
-        auto endTime = std::chrono::high_resolution_clock::now();
-        auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(endTime - startTime);
+  cout << "Reconstructing Coin Change Tests ---------------" << endl << endl;
 
-        // Print the result and the time taken for each test
-        std::cout << "Result: " << result << std::endl;
-        std::cout << "Time taken: " << duration.count() << " nanoseconds" << std::endl;
+  int reconstruction_tests[] {10, 25, 50, 100, 200, 500};
+
+  for (int i = 0; i < sizeof(reconstruction_tests) / sizeof(reconstruction_tests[0]); ++i) {
+    auto startTime = std::chrono::high_resolution_clock::now();
+
+      displayCoinCombinations(reconstruction_tests[i], us_coins, num_us_coins);
+
+
+    auto endTime = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(
+        endTime - startTime);
+
+    // Print the result and the time taken for each test
+    std::cout << "Experiment input: " << reconstruction_tests[i] << std::endl;
+    std::cout << "Result: ";
+    for (int j = 0; j < result.size(); ++j) {
+      std::cout << result[j] << " ";
     }
+    std::cout << std::endl;
+    std::cout << "Time taken: " << duration.count() << " nanoseconds"
+              << std::endl;
+  }  
+
 
   return 0;
 }
